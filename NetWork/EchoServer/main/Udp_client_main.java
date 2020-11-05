2c7533e07f32bb55b70da26eaba048658459af53
bhj03245 <bhj03245@naver.com>
2020-11-05 18:16:47 +0900

    Java Network Programming

---
+++ NetWork/EchoServer/main/Udp_client_main.java
@@ -0,0 +1,12 @@
+package test.main;
+
+import test.udp.UDPEchoClient;
+
+public class Udp_client_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new UDPEchoClient().clientUDP("localhost");
+	}
+
+}
---
+++ NetWork/EchoServer/main/Udp_server_main.java
@@ -0,0 +1,12 @@
+package test.main;
+
+import test.udp.UDPEchoServer;
+
+public class Udp_server_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new UDPEchoServer().serverUDP(9007);
+	}
+
+}
